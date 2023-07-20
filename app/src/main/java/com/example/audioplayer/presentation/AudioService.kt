e5a47947714bbcad43c192b2114c8de13eb576fb
ilyapc <ilya.komendantov@list.ru>
2023-07-20 09:24:31 +0300

    add-seekTo

--- app/src/main/java/com/example/audioplayer/presentation/AudioService.kt
+++ app/src/main/java/com/example/audioplayer/presentation/AudioService.kt
@@ -6,15 +6,18 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.media.MediaMetadata
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.IBinder
 import android.provider.MediaStore
-import android.widget.RemoteViews
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.example.audioplayer.R
-import com.example.audioplayer.core.log
 
 class AudioService : Service() {
 
@@ -28,6 +31,7 @@ class AudioService : Service() {
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var notification: NotificationCompat.Builder
+    private lateinit var mediaSession: MediaSessionCompat
 
     override fun onCreate() {
         super.onCreate()
@@ -76,10 +80,12 @@ class AudioService : Service() {
 
                 if (isRun) {
                     mediaPlayer.pause()
-                    notificationBuild.actions[1] = Notification.Action(R.drawable.ic_start, "pause", pausePendingIntent)
+                    notificationBuild.actions[1] =
+                        Notification.Action(R.drawable.ic_start, "pause", pausePendingIntent)
                 } else {
                     mediaPlayer.start()
-                    notificationBuild.actions[1] = Notification.Action(R.drawable.ic_pause, "pause", pausePendingIntent)
+                    notificationBuild.actions[1] =
+                        Notification.Action(R.drawable.ic_pause, "pause", pausePendingIntent)
                 }
 
                 isRun = !isRun
@@ -87,18 +93,23 @@ class AudioService : Service() {
             }
 
             "skipPrevious" -> {
+                setPlaybackState()
                 if (currentMusic == 0) currentMusic = musics.size - 1
                 else currentMusic--
                 val data = musics[currentMusic].toString()
                 updateNotification(data)
                 settingMediaPlayer()
+                setMetadata()
             }
 
             "skipNext" -> {
-                checkLastMusic()
+                setPlaybackState()
+                if (currentMusic == musics.size - 1) currentMusic = 0
+                else currentMusic++
                 val data = musics[currentMusic].toString()
                 updateNotification(data)
                 settingMediaPlayer()
+                setMetadata()
             }
 
             "stop" -> {
@@ -108,8 +119,10 @@ class AudioService : Service() {
             else -> {
                 val data = intent?.getStringExtra("TITLE") ?: ""
                 if (isRun) {
+                    setPlaybackState()
                     settingMediaPlayer(data)
                     updateNotification(data)
+                    setMetadata()
                 } else {
                     mediaPlayer.setOnCompletionListener {
                         val skipNext = Intent(this, AudioService::class.java).apply {
@@ -129,18 +142,37 @@ class AudioService : Service() {
         return START_NOT_STICKY
     }
 
-    private fun checkLastMusic() {
-        if (currentMusic == musics.size - 1) currentMusic = 0
-        else currentMusic++
+    private fun setPlaybackState(seekTo: Long? = null) {
+        var position = 0L
+        if (seekTo != null) position = seekTo
+        mediaSession.setPlaybackState(
+            PlaybackStateCompat.Builder().setState(
+                PlaybackStateCompat.STATE_PLAYING,
+                position,
+                1f
+            )
+                .setActions(PlaybackStateCompat.ACTION_SEEK_TO)
+                .build()
+        )
     }
+
+    private fun setMetadata() {
+        val metadata = MediaMetadataCompat.Builder()
+            .putLong(MediaMetadata.METADATA_KEY_DURATION, mediaPlayer.duration.toLong())
+            .build()
+        mediaSession.setMetadata(metadata)
+    }
+
     private fun updateNotification(data: String) {
         setContentTitleOnNotification(data)
         notificationManager.notify(1, notification.build())
     }
 
     private fun setContentTitleOnNotification(data: String) {
-        notification.setContentTitle(data.substring(data.lastIndexOf("/") + 1, data.lastIndexOf("."))
-            .replace("_", " "))
+        notification.setContentTitle(
+            data.substring(data.lastIndexOf("/") + 1, data.lastIndexOf("."))
+                .replace("_", " ")
+        )
     }
 
     private fun createNotification(): NotificationCompat.Builder {
@@ -172,9 +204,26 @@ class AudioService : Service() {
         val stopPendingIntent =
             PendingIntent.getService(this, 4, stop, PendingIntent.FLAG_UPDATE_CURRENT)
 
+        mediaSession = MediaSessionCompat(this, "TAG")
+        setMetadata()
+        setPlaybackState()
+
+        mediaSession.setCallback(object : MediaSessionCompat.Callback() {
+            override fun onSeekTo(pos: Long) {
+                mediaPlayer.seekTo(pos.toInt())
+                setPlaybackState(pos)
+            }
+        })
+
         return NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_background)
-            .setStyle(androidx.media.app.NotificationCompat.MediaStyle())
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setCategory(NotificationCompat.CATEGORY_PROGRESS)
+            .setColor(Color.WHITE)
+            .setStyle(
+                androidx.media.app.NotificationCompat.MediaStyle()
+                    .setShowActionsInCompactView(0, 1, 2).setMediaSession(mediaSession.sessionToken)
+            )
             .addAction(
                 R.drawable.ic_skip_previous,
                 getString(R.string.skip_previous),
